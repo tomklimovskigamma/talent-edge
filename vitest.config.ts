32d8db94bf6b22ae84e091088b8b92a203519a71
tomklimovskigamma <tomklimovski@gammadata.io>
2026-04-14 11:52:39 +1000

    fix: use vite-tsconfig-paths plugin correctly in vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from "vitest/config";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  resolve: {
-    tsconfigPaths: true,
-  },
+  plugins: [tsconfigPaths()],
   test: {
     environment: "node",
+    passWithNoTests: true,
   },
 });
